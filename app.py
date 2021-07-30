01bb8feddaeca9a9c00f718a42353e683c602b6b
nsapse <nsapse@gmail.com>
2021-07-30 09:22:12 -0400

    added pokemon endpoint

--- app.py
+++ app.py
@@ -24,14 +24,24 @@ def return_img_url(search_string):
     :returns: A JSON object containing {img_url: [the image url]}
 
     """
-    data = {'img_url': getImageURL(search_string)}
+    data = {'img_url': getGenericImageURL(search_string)}
     return jsonify(data)
 
+
+@app.route("/pokemon/<search_string>")
+def return_pokemon_url(search_string):
+    """Route function which takes the query string being passed as part
+    of the route and calls getImageURL then returns the URL as JSON.
+    :returns: A JSON object containing {img_url: [the image url]}
+
+    """
+    data = {'img_url': getPokemonImageURL(search_string)}
+    return jsonify(data)
 
 # primary function for getting and returning strings - called by flask
 
 
-def getImageURL(search_string):
+def getGenericImageURL(search_string):
     """getImageURL - given a search string returns the URL to an image
     from IMGUR related to the string
 
@@ -43,23 +53,60 @@ def getImageURL(search_string):
     # # link to image of OSU beaver for failed queries
     # failure_image = 'https: // upload.wikimedia.org/wikipedia/en/thumb/1/1b/Oregon_State_Beavers_logo.svg/1200px-Oregon_State_Beavers_logo.svg.png'
 
-    # generate the actual search query
+    # generate the actual search query and return it
+
     imgur_base = 'https://www.imgur.com/search?q='
-    search_query = imgur_base + search_string
+    search_return = executeQuery(imgur_base, search_string)
 
-    # execute the search
-    search_return = requests.get(search_query)
+    # return default image in case of failure
+    if search_return[1] is False:
+        return search_return[0]
 
-    # if the search fails return the failure image by default
-    if search_return.status_code != 200:
-        return failure_image
-
-    # otherwise get all image links and return the first one
-    # (actually, the third, the first two image links are logos)
-
-    searchable_return = BeautifulSoup(search_return.content, 'html.parser')
-    img_links = searchable_return.find_all('img')
+    img_links = search_return[0].find_all('img')
 
     agnostic_link = img_links[3]['src']
 
     return 'https:' + agnostic_link
+
+
+def getPokemonImageURL(pokemon):
+    """getPokemonImageURL- given a search string returns the URL to an image
+    of a Pokemon from Wikipedia related to the string
+
+    :search_string: the string describing the subject of the
+    image sought (MUST BE A POKEMON)
+
+    :returns: str -> imgURL
+
+    """
+
+    wiki_base = 'https://en.wikipedia.org/wiki/'
+    search_return = executeQuery(wiki_base, pokemon)
+
+    # return default image in case of failure
+    if search_return[1] is False:
+        return search_return[0]
+
+    images = search_return[0].find_all('img')
+    image_link = images[0]['src']
+
+    print('https:' + image_link)
+    return 'https:' + image_link
+
+
+def executeQuery(baseURL: str, searchSTR: str):
+
+    # build the search URL
+    searchURL = baseURL + searchSTR
+
+    # execute the actual request
+    search_return = requests.get(searchURL)
+    if search_return.status_code != 200:
+        return (failure_image, False)
+
+    # otherwise get all image links and return the first one
+    # (actually, the third, the first two image links are logos)
+
+    parsed_content = BeautifulSoup(search_return.content, 'html.parser')
+
+    return (parsed_content, True)
